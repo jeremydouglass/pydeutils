aeaef8bee594c168eebcaa24cdcd49f9188013b3
Jeremy Douglass <jeremydouglass@gmail.com>
2017-12-13 15:32:56 -0800

    add @at_exit
    mapped from atexit.register

--- pydeutils/__init__.py
+++ pydeutils/__init__.py
@@ -1,3 +1,9 @@
 from decorators import disable, enable, handle_file, print_args
 from fileutils import exists
 from pickler import loadPickle, deletePickle, savePickle
+
+
+# map builtins
+
+from atexit import register as at_exit
+# docs.python.org/2.7/library/atexit.html#atexit.register
